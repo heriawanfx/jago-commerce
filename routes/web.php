fe92c143733821e2110fe7c27d287db930f05142
wawan-mis <wawan-mis@ams.co.id>
2023-08-18 16:34:21 +0700

    Refactor homepage, add userSeeder

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('blank-page', ['type_menu' => '']);
+    return view('pages.blank-page', ['type_menu' => '']);
 });
 
 
