42cd5c761c296d00d758d8e9c94f6e4eeece6775
wawan-mis <wawan-mis@ams.co.id>
2023-08-24 14:17:41 +0700

    Add Product & Category

---
+++ database/migrations/2023_08_24_062056_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->double('price');
+            $table->string('image_url');
+            $table->timestamps();
+
+            $table->foreignIdFor(Category::class); //this will add category_id column
+            $table->foreignIdFor(User::class); //this will add user_id column
+
+            //Don't forget to add foreign fillable columns to Product model
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
