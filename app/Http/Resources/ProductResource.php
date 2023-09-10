ede294d13bb1de4293d582f16205834ce96e0ea1
heriawanfx <heriawan.fx@gmail.com>
2023-09-10 17:17:58 +0700

    Refactor Resource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -18,9 +18,9 @@ class ProductResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'descripton' => $this->description,
+            'description' => $this->description,
             'price' => $this->price,
-            'image_product' => $this->image_url,
+            'image_url' => $this->image_url,
             'category' => new CategoryResource($this->whenLoaded('category')),
             'user' => new UserResource($this->whenLoaded('user')),
         ];
